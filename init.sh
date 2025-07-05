c3cc9d2d734380aa81be4b02628373d8956a09fb
rithgroove <rithgroove@gmail.com>
2025-07-05 09:43:33 +0700

    don't use init, because the goal is to develop it from workspace (resolves #1, resolves #2)

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 
-echo downloading libraries
-mkdir -p external
-cd ./external
+# echo downloading libraries
+# mkdir -p external
+# cd ./external
 
-echo cloning retronomicon
+# echo cloning retronomicon
 
-git clone git@github.com:rithgroove/retronomicon.git retronomicon
-cd ./retronomicon
-USE_LUA=ON USE_PYTHON=ON  ./init.sh
-cd ..
+# git clone git@github.com:rithgroove/retronomicon.git retronomicon
+# cd ./retronomicon
+# USE_LUA=ON USE_PYTHON=ON  ./init.sh
+# cd ..
 
